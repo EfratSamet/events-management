4b1f90381fa99782d5d7e304b7ff70fb09797005
EfratSamet <sametfamily21@gmail.com>
2025-03-03 21:52:59 +0200

    stam

---
+++ Repository/Interfaces/IOrganizerRepository.cs
@@ -0,0 +1,15 @@
+﻿using Repository.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IOrganizerRepository:IRepository<Organizer>
+    {
+        List<Event> GetEventsByOrganizerId(string organizerId);
+        List<Group> GetGroupsByOrganizerId(string organizerId);
+    }
+}
---
+++ Service/Interfaces/IOrganizerService.cs
@@ -0,0 +1,16 @@
+﻿using Repository.Entity;
+using Service.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IOrganizerService:IService<OrganizerDto>
+    {
+        List<EventDto> GetEventsByOrganizerId(string id);
+        List<GroupDto> GetGroupsByOrganizerId(string id);
+    }
+}
